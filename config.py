4be224b3ba794532a0800bbefc6d2a20f894e1ce
Du Duong <khaidu2002@gmail.com>
2020-09-16 22:40:58 -0500

    Started on Config and Game file

---
+++ config.py
@@ -0,0 +1,7 @@
+# please replace entities in quotes with your own API.
+
+
+api_key = "YOUR_KEY"
+api_secret = "YOUR_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+token_secret = "YOUR_TOKEN_SECRET"
---
+++ game.py
@@ -0,0 +1,9 @@
+import config
+import tweepy
+import re
+
+kanye_id = 169686021
+elon_id = 44196397
+
+kanye_tweets = []
+elon_tweets = []
