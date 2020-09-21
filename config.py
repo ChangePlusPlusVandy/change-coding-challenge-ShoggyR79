779514bd22ec32ce4059ba5feb9af3b10c41f7a8
Du Duong <khaidu2002@gmail.com>
2020-09-20 21:28:14 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # please replace entities in quotes with your own API.
 
 
-api_key = "YOUR-KEY"
+api_key = "YOUR_KEY"
 api_secret = "YOUR_API_SECRET"
 access_token = "YOUR_ACCESS_TOKEN"
 token_secret = "YOUR_ACCESS_TOKEN_SECRET"
